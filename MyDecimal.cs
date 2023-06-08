ca2475f0f40db4f044a68e31d37d738e43b9bfa8
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-06-08 12:36:13 +0300

    Add files via upload
    Перше завантаження на GitHub

---
+++ MyDecimal.cs
@@ -0,0 +1,282 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace MyLibraries.MySystemLib.Classes
+{
+    /// <summary>
+    /// Десяткове число з плаваючою комою
+    /// </summary>
+    static public class MyDecimal
+    {
+        #region Items
+        /// <summary>
+        /// Список розподілювачів за замовчуванням
+        /// </summary>
+        public static readonly List<string> DefaultListSeparators = new List<string>() { ".", "," };
+        #endregion Items
+
+        #region Functions
+        #region Parse
+        /// <summary>
+        /// Перетворити текст на число
+        /// </summary>
+        /// <param name="number">Поточне число</param>
+        /// <param name="numberToString">Текст</param>
+        /// <returns>true - вдалося перетворити, false - не вдалося перетворити</returns>
+        public static bool Parse(ref decimal number, string numberToString)
+        {
+            if (numberToString.Length == 0) return false;
+
+            try
+            {
+                number = decimal.Parse(numberToString, new NumberFormatInfo() { NumberDecimalSeparator = "," });
+            }
+            catch
+            {
+                try
+                {
+                    number = decimal.Parse(numberToString, new NumberFormatInfo() { NumberDecimalSeparator = "." });
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+        #endregion Parse
+
+        #region Check
+        /// <summary>
+        /// Перевірити чи можливе перетворення тексту на число
+        /// </summary>
+        /// <param name="decimalToString">Текст</param>
+        /// <returns>true - вдалося перетворити, false - не вдалося перетворити</returns>
+        public static bool CheckParse(string decimalToString)
+        {
+            decimal currentDecimal = default;
+
+            return Parse(ref currentDecimal, decimalToString);
+        }
+        /// <summary>
+        /// Перевірити точність числа
+        /// </summary>
+        /// <param name="checkNumber">Число для перевірки</param>
+        /// <param name="referenceNumber">Еталонне число</param>
+        /// <returns>true - число коректне, false - число не коректне</returns>
+        static public bool CheckPrecision(decimal checkNumber, decimal referenceNumber)
+        {
+            #region Items
+            const int defaultForPrecision = -1;
+            List<string> listSeparators = new List<string>() { ".", "," };
+            int
+                precisionReferenceNumber = defaultForPrecision,
+                precisionCheckNumber = 0;
+            #endregion Items
+
+            GetPrecision(ref precisionReferenceNumber, referenceNumber, listSeparators);
+            if (precisionReferenceNumber == defaultForPrecision) return false;
+
+            GetPrecision(ref precisionCheckNumber, checkNumber, listSeparators);
+            return !(precisionCheckNumber > precisionReferenceNumber);
+        }
+        #endregion Check
+
+        #region Sets
+        /// <summary>
+        /// Встановити точність
+        /// </summary>
+        /// <param name="number">Поточне число</param>
+        /// <param name="referenceNumber">Еталонне число</param>
+        static public void SetPrecision(ref decimal number, decimal referenceNumber)
+        {
+            if (number == default || referenceNumber == default) return;
+
+            #region Items
+            List<string> listSeparators = new List<string>() { ".", "," };
+            int precision = -1;
+
+            GetPrecision(ref precision, referenceNumber, listSeparators);
+            #endregion Items
+
+            if (precision != -1)
+                SetPrecision(ref number, precision, listSeparators);
+            else
+                TakeFractionalPart(ref number, listSeparators);
+        }
+        /// <summary>
+        /// Встановити точність
+        /// </summary>
+        /// <param name="number">Поточне число</param>
+        /// <param name="precision">Точність після коми</param>
+        /// <param name="listSeparators">Список розділювачів</param>
+        static public void SetPrecision(ref decimal number, int precision, List<string> listSeparators)
+        {
+            if (number == default || !(precision > 0)) return;
+
+            #region Items
+            string
+                numberToString = number.ToString(),
+                newNumericToString = default,
+                currentSymbol = default;
+            int lengthNumericToString = numberToString.Length;
+            #endregion Items
+
+            for (int i = 0; i < lengthNumericToString; i++)
+            {
+                currentSymbol = numberToString[i].ToString();
+
+                if (MyList.CheckValueForEntryInList(currentSymbol, listSeparators))
+                {
+                    int length = precision + 1;
+
+                    if (length < lengthNumericToString - i)
+                    {
+                        newNumericToString += numberToString.Substring(i, length);
+                        number = decimal.Parse(newNumericToString);
+                    }
+
+                    return;
+                }
+
+                newNumericToString += currentSymbol;
+            }
+        }
+        /// <summary>
+        /// Встановити точність
+        /// </summary>
+        /// <param name="number">Поточне число</param>
+        /// <param name="precision">Точність після коми</param>
+        static public void SetPrecision(ref decimal number, int precision) =>
+            SetPrecision(ref number, precision, DefaultListSeparators);
+        #endregion Sets
+
+        #region Gets
+        #region Get precision
+        /// <summary>
+        /// Отримати точність числа
+        /// </summary>
+        /// <param name="precision">Поточна точність</param>
+        /// <param name="numberToString">Число у вигляді рядка</param>
+        /// <param name="listSeparators">Список розділювачів</param>
+        static public void GetPrecision(ref int precision, string numberToString, List<string> listSeparators)
+        {
+            int lengthNumberic = numberToString.Length;
+
+            for (int i = 0; i < lengthNumberic; i++)
+                if (MyList.CheckValueForEntryInList(numberToString[i].ToString(), listSeparators))
+                {
+                    precision = numberToString.Length - 1 - i; return;
+                }
+        }
+        /// <summary>
+        /// Отримати точність числа
+        /// </summary>
+        /// <param name="precision">Поточна точність</param>
+        /// <param name="number">Число у вигляді рядка</param>
+        /// <param name="listSeparators">Список розділювачів</param>
+        static public void GetPrecision(ref int precision, decimal number, List<string> listSeparators)
+        {
+            TakeExtraZerosFractionalPart(ref number);
+
+            GetPrecision(ref precision, number.ToString(), listSeparators);
+        }
+        /// <summary>
+        /// Отримати точність числа
+        /// </summary>
+        /// <param name="precision">Поточна точність</param>
+        /// <param name="number">Число у вигляді рядка</param>
+        static public void GetPrecision(ref int precision, decimal number) =>
+            GetPrecision(ref precision, number, DefaultListSeparators);
+        #endregion Get precision
+
+        #region Get digit
+        /// <summary>
+        /// Отримати розрядність числа
+        /// </summary>
+        /// <param name="digit">Поточна розрядність</param>
+        /// <param name="numberToString">Число у вигляді рядка</param>
+        /// <param name="listSeparators">Список розділювачів</param>
+        static public void GetDigit(ref int digit, string numberToString, List<string> listSeparators)
+        {
+            int lenghtNumberToString = numberToString.Length;
+
+            try
+            {
+                if (!(lenghtNumberToString > 0)) throw new Exception();
+
+                for (int i = 0; i < lenghtNumberToString; i++)
+                    if (MyList.CheckValueForEntryInList(numberToString[i].ToString(), listSeparators))
+                    {
+                        digit = i; return;
+                    }
+
+                throw new Exception();
+            }
+            catch { digit = lenghtNumberToString; }
+        }
+        /// <summary>
+        /// Отримати розрядність числа
+        /// </summary>
+        /// <param name="digit">Поточна розрядність</param>
+        /// <param name="number">Число</param>
+        /// <param name="listSeparators">Список розділювачів</param>
+        static public void GetDigit(ref int digit, decimal number, List<string> listSeparators) =>
+            GetDigit(ref digit, number.ToString(), listSeparators);
+        /// <summary>
+        /// Отримати розрядність числа
+        /// </summary>
+        /// <param name="digit">Поточна розрядність</param>
+        /// <param name="number">Число</param>
+        static public void GetDigit(ref int digit, decimal number) =>
+            GetDigit(ref digit, number, DefaultListSeparators);
+        #endregion Get digit
+        #endregion Gets
+
+        #region Takes
+        /// <summary>
+        /// Забрати дробову частину
+        /// </summary>
+        /// <param name="number">Поточне число</param>
+        /// <param name="listSeparators">Список розділювачів</param>
+        static public void TakeFractionalPart(ref decimal number, List<string> listSeparators)
+        {
+            if (number == default) return;
+
+            string
+                numberToString = number.ToString(),
+                newNumberToString = default,
+                currentSymbol = default;
+            int lengthNumberToString = numberToString.Length;
+
+            for (int i = 0; i < lengthNumberToString; i++)
+            {
+                currentSymbol = numberToString[i].ToString();
+
+                if (MyList.CheckValueForEntryInList(currentSymbol, listSeparators))
+                {
+                    number = decimal.Parse(newNumberToString);
+                    break;
+                }
+
+                newNumberToString += currentSymbol;
+            }
+        }
+        /// <summary>
+        /// Забрати дробову частину. Як список розподілювачів буде використано MyDecimal.DefaultListSeparators
+        /// </summary>
+        /// <param name="number">Поточне число</param>
+        static public void TakeFractionalPart(ref decimal number) =>
+            TakeFractionalPart(ref number, DefaultListSeparators);
+        /// <summary>
+        /// Забрати зайві нулі з дробової частини числа
+        /// </summary>
+        /// <param name="number">Поточне число</param>
+        static public void TakeExtraZerosFractionalPart(ref decimal number) =>
+            number = Convert.ToDecimal(Convert.ToDouble(number));
+        #endregion Takes
+        #endregion Functions
+    }
+}
---
+++ MyList.cs
@@ -0,0 +1,302 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyLibraries.MySystemLib.Classes
+{
+    /// <summary>
+    /// Список
+    /// </summary>
+    static public class MyList
+    {
+        #region Add
+        /// <summary>
+        /// Добавити значення в List
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list">Поточний List</param>
+        /// <param name="newValue">Нове значення</param>
+        /// <param name="index">Індекс нового значення. Якщо менше за 0 або більше за кількість значень в списку - добавляється в кінець</param>
+        static public void Add<T>(ref List<T> list, T newValue, int index = 0)
+        {
+            List<T> newList = new List<T>();
+            int countList = list.Count;
+
+            if (index < 0 || index > countList - 1)
+            {
+                newList = list;
+                newList.Add(newValue);
+            }
+            else
+            {
+                for (int i = 0; i < countList; i++)
+                {
+                    if (i == index) newList.Add(newValue);
+                    newList.Add(list[i]);
+                }
+            }
+
+            list = newList;
+        }
+        /// <summary>
+        /// Добавити в список значення з іншого списку
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="currentList">Поточний список</param>
+        /// <param name="addList">Додатковий список</param>
+        /// <param name="index">Індекс додавання</param>
+        static public void Add<T>(ref List<T> currentList, List<T> addList, int index = 0)
+        {
+            int countAddList = addList.Count;
+            if (countAddList == 0) return;
+
+            List<T> newList = currentList;
+
+            for (int i = 0; i < countAddList; i++) Add(ref newList, addList[i], index);
+
+            currentList = newList;
+        }
+        /// <summary>
+        /// Добавити список списків в список
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="currentList">Поточний список</param>
+        /// <param name="addArrayList">Список списків</param>
+        /// <param name="index">Індекс додавання</param>
+        static public void Add<T>(ref List<T> currentList, List<List<T>> addArrayList, int index = 0)
+        {
+            int countAddArrayList = addArrayList.Count;
+            if (countAddArrayList == 0) return;
+
+            List<T> newList = currentList;
+
+            for (int i = 0; i < countAddArrayList; i++) Add(ref newList, addArrayList[i], index);
+
+            currentList = newList;
+        }
+        #endregion Add
+
+        #region Replace
+        /// <summary>
+        /// Замінити значення в списку за індексом
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list">Поточний список</param>
+        /// <param name="newValue">Нове значення</param>
+        /// <param name="index">Індекс значення. Якщо значення за велике - дія не виконується</param>
+        static public void Replace<T>(ref List<T> list, T newValue, int index)
+        {
+            int countList = list.Count;
+            if (index < 0 || index > countList - 1) return;
+
+            List<T> newList = new List<T>();
+            T currentValue;
+
+            for (int i = 0; i < countList; i++)
+            {
+                currentValue = list[i];
+
+                if (i == index) currentValue = newValue;
+
+                newList.Add(currentValue);
+            }
+
+            list = newList;
+        }
+        #endregion Replace
+
+        #region Remove
+        /// <summary>
+        /// Видалити елемент(-и) за вказаним індексом. Видалення з primaryIndex по finalIndex
+        /// </summary>
+        /// <param name="list">Поточний список</param>
+        /// <param name="primaryIndex">Початковий індекс</param>
+        /// <param name="finalIndex">Кінцевий індекс. Якщо 0 finalIndex = primaryIndex</param>
+        static public void Remove<T>(ref List<T> list, int primaryIndex, int finalIndex = 0)
+        {
+            #region Items
+            int countList = list.Count;
+
+            if (finalIndex == 0) finalIndex = primaryIndex;
+            if (!(primaryIndex > -1) || !(finalIndex < countList) || primaryIndex > finalIndex) return;
+
+            List<T> newList = new List<T>();
+            #endregion Items
+
+            for (int i = 0; i < countList; i++)
+            {
+                if (i == primaryIndex)
+                {
+                    for (i = finalIndex + 1; i < countList; i++) newList.Add(list[i]);
+
+                    break;
+                }
+
+                newList.Add(list[i]);
+            }
+
+            list = newList;
+        }
+        /// <summary>
+        /// Видалити дублікати
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list">Поточний список</param>
+        static public void RemoveDuplicates<T>(ref List<T> list)
+        {
+            #region Items
+            List<T> newList = new List<T>();
+            T currentElement = default;
+            bool currentElementExist = default;
+            int countList = list.Count;
+            #endregion Items
+
+            for (int i = 0; i < countList; i++)
+            {
+                currentElementExist = false;
+                currentElement = list[i];
+
+                for (int j = 0; j < newList.Count; j++)
+                {
+                    if (MyMath.CheckForEquality(currentElement, newList[j]))
+                    {
+                        currentElementExist = true;
+                        break;
+                    }
+                }
+
+                if (!currentElementExist) newList.Add(currentElement);
+            }
+
+            list = newList;
+        }
+        #endregion Remove
+
+        #region Get
+        /// <summary>
+        /// Отримати індекс значення в списку. Якщо значення в списку не знайдено, значення параметра currentIndex не змнінюється
+        /// </summary>
+        /// <param name="currentIndex">Поточний індекс</param>
+        /// <param name="list">Список</param>
+        /// <param name="value">Значення</param>
+        static public void GetIndexValue<T>(ref int currentIndex, List<T> list, T value)
+        {
+            int countList = list.Count;
+            if (countList == 0) return;
+
+            for (int i = 0; i < countList; i++)
+                if (MyMath.CheckForEquality(list[i], value))
+                {
+                    currentIndex = i;
+                    break;
+                }
+        }
+        /// <summary>
+        /// Отримати список англійських літер нижнього регістру
+        /// </summary>
+        /// <param name="englishLowercaseLetters">Поточний список</param>
+        static public void GetEnglishLowercaseLetters(ref List<string> englishLowercaseLetters) =>
+            englishLowercaseLetters = new List<string>()
+            {
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"
+            };
+        /// <summary>
+        /// Отримати список англійських літер верхнього регістру регістру
+        /// </summary>
+        /// <param name="englishUppercaseLetters">Поточний список</param>
+        static public void GetEnglishUppercaseLetters(ref List<string> englishUppercaseLetters) =>
+            englishUppercaseLetters = new List<string>()
+            {
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+            };
+        /// <summary>
+        /// Отримати список всіх англійських літер
+        /// </summary>
+        /// <param name="englishLetters">Поточний список</param>
+        static public void GetEnglishLetters(ref List<string> englishLetters) =>
+            englishLetters = new List<string>()
+            {
+                "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+            };
+        /// <summary>
+        /// Отримати список цифр 0-9
+        /// </summary>
+        /// <param name="numbers">Поточний список</param>
+        static public void GetNumbers(ref List<string> numbers) =>
+            numbers = new List<string>()
+            {
+                "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"
+            };
+        #endregion Get
+
+        #region Checks
+        /// <summary>
+        /// Перевірити входження значення в список 
+        /// </summary>
+        /// <param name="value">Значення для пошуку</param>
+        /// <param name="listValues">Список значень</param>
+        /// <returns>true - значення знайдено в списку, false - значення не знайдено в списку</returns>
+        static public bool CheckValueForEntryInList<T>(T value, List<T> listValues)
+        {
+            int countListValues = listValues.Count;
+            if (value == null || countListValues == 0) return false;
+
+            for (int i = 0; i < countListValues; i++)
+                if (MyMath.CheckForEquality(value, listValues[i])) return true;
+
+            return false;
+        }
+        /// <summary>
+        /// Перевірити на недозволені рядки в списку
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="listValue">Поточний список</param>
+        /// <param name="allowedValue">Список дозволених значень</param>
+        /// <returns>true - недозволене значення знайдено, false - недозволене значення не знайдено</returns>
+        static public bool CheckForIllegalValueInList<T>(List<T> listValue, List<T> allowedValue)
+        {
+            int
+                countListValue = listValue.Count,
+                countAllowedValue = allowedValue.Count;
+            if (countListValue == 0 || countAllowedValue == 0) return true;
+
+            for (int i = 0; i < countListValue; i++)
+                if (!CheckValueForEntryInList(listValue[i], allowedValue)) return true;
+
+            return false;
+        }
+        /// <summary>
+        /// Перевірити індекс на наявність у списку
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="list">Поточний список</param>
+        /// <param name="index">Поточний індекс</param>
+        /// <returns>true - індекс входить у список, false - індекс не входить у список</returns>
+        static public bool CheckIndexForOccurrence<T>(List<T> list, int index)
+        {
+            return !(index < 0) && index < list.Count;
+        }
+        #endregion Checks
+
+        #region Sort
+        /// <summary>
+        /// Відсортувати. Використовується для списків з декількома типами
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <typeparam name="TKey">Ключ для сортування</typeparam>
+        /// <param name="list">Список значень</param>
+        /// <param name="keySelector">Ключ для сортування</param>
+        /// <param name="byGrowth">true - сортувати по зростанню, false - сортувати по спаданню</param>
+        static public void Sort<T, TKey>(ref List<T> list, Func<T, TKey> keySelector, bool byGrowth = true)
+        {
+            IOrderedEnumerable<T> resultSort = default;
+
+            if (byGrowth) resultSort = list.OrderBy(keySelector);
+            else resultSort = list.OrderByDescending(keySelector);
+
+            list = resultSort.ToList();
+        }
+        #endregion Sort
+    }
+}
