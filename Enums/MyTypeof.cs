f36fa57f808328e5040027364fe5249375ca5864
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-06-16 17:56:57 +0300

    Add files via upload

---
+++ Enums/MyTypeof.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibraries.MySystemLib.Enums
+{
+    /// <summary>
+    /// Перерахування типів 
+    /// </summary>
+    enum MyTypeof
+    {
+        @other = 0,
+        @object = 1,
+        @char = 2,
+        @string = 3,
+        @int = 4,
+        @double = 5,
+        @float = 6,
+        @decimal = 7
+    }
+}
