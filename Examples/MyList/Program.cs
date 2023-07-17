996fd56eee8ebf7543af58e141cf40d1b0b1931a
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-17 15:09:37 +0300

    Add files via upload

---
+++ Examples/MyList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            List<string> listValues = new List<string>();
+            IEnumerable<string> enumerableRemove = default;
+            #endregion Items
+
+            #region Filling
+            listValues.Add("m");
+            listValues.Add("f");
+            listValues.Add("y");
+            listValues.Add("k");
+            listValues.Add("o");
+            listValues.Add("a");
+            listValues.Add("q");
+            listValues.Add("z");
+            listValues.Add("p");
+            listValues.Add("w");
+
+            enumerableRemove = new List<string>() { "a", "o", "e", "y", "u" };
+            #endregion Filling
+
+            // Start listValues:        {"m", "f", "y", "k", "o", "a", "q", "z", "p", "w"}
+            // Info enumerableRemove:   {"a", "o", "e", "y", "u"}
+
+            MyList.Remove(ref listValues, enumerableRemove);
+
+            // Finish listValues: {"m", "f", "k", "q", "z", "p", "w"}
+
+            return;
+        }
+    }
+}
