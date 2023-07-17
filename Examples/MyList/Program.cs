db67c5aa6f20d196480f355b894564100afc8dcd
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-17 20:02:00 +0300

    Add files via upload

---
+++ Examples/MyList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            bool result = default;
+            List<int> list = new List<int>();
+            #endregion Items
+
+            #region Filling
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            #endregion Filling
+
+            // Start list: {1, 2, 3, 4, 5}
+
+            result = MyList.IsEntry(list, 5); // result = false
+            result = MyList.IsEntry(list, 4); // result = true
+
+            return;
+        }
+    }
+}
