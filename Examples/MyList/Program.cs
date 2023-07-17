8dde60f9fd2287ccf7da249abdeeab409f369afb
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-17 21:06:35 +0300

    Add files via upload

---
+++ Examples/MyList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            bool result = default;
+            List<string>
+                emptyList = new List<string>(),
+                notEmptyList = new List<string>();
+            #endregion Items
+
+            #region Filling
+            notEmptyList.Add("hello");
+            notEmptyList.Add("start");
+            notEmptyList.Add("middle");
+            notEmptyList.Add("bye");
+            notEmptyList.Add("finish");
+            #endregion Filling
+
+            // Start emptyList:     {}
+            // Start notEmptyList:  {"hello", "start", "middle", "bye", "finish"}
+
+            result = MyList.IsEmpty(emptyList);     // result = true
+            result = MyList.IsEmpty(notEmptyList);  // result = false
+
+            return;
+        }
+    }
+}
