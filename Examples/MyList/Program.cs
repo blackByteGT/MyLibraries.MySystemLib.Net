68c71dfcfd6cf2ad6abf4a803b1f91c09d7196d8
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-17 18:40:55 +0300

    Add files via upload

---
+++ Examples/MyList/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            int count = default;
+            List<string>
+                listString = new List<string>(),
+                listMiddle = new List<string>(),
+                listBegin = new List<string>(),
+                listEnd = new List<string>();
+            #endregion Items
+
+            #region Filling
+            listString.Add("hello");
+            listString.Add("start");
+            listString.Add("finish");
+            listString.Add("return");
+            listString.Add("main");
+            listString.Add("class");
+            listString.Add("public");
+            listString.Add("bye");
+
+            count = listString.Count / 2;
+            #endregion Filling
+
+            // Start listString:   {"hello", "start", "finish", "return", "main", "class", "public", "bye"}
+
+            MyList.GetSublist(ref listMiddle, listString, ((listString.Count - count) / 2), count);
+            MyList.GetSublist(ref listBegin, listString, count: count);
+            MyList.GetSublist(ref listEnd, listString, index: listString.Count - count);
+
+            // Info listMiddle: {"finish", "return", "main", "class"}
+            // Info listBegin:  {"hello", "start", "finish", "return"}
+            // Info listEnd:    {"main", "class", "public", "bye"}
+
+            return;
+        }
+    }
+}
