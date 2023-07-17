fdb2042a343aebaffc86152e4585247018fc7bd4
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-17 14:46:00 +0300

    Add files via upload

---
+++ Examples/MyList/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listString = new List<string>();
+
+            #region Filling
+            listString.Add("m");
+            listString.Add("f");
+            listString.Add("y");
+            listString.Add("k");
+            listString.Add("o");
+            listString.Add("a");
+            listString.Add("q");
+            listString.Add("z");
+            listString.Add("p");
+            listString.Add("w");
+            #endregion Filling
+
+            // Start listString:    {"m", "f", "y", "k", "o", "a", "q", "z", "p", "w"}
+
+            MyList.Remove(ref listString, 1);                       // {"m", "y", "k", "o", "a", "q", "z", "p", "w"}
+            MyList.Remove(ref listString, 3, listString.Count - 1); // {"m", "y", "k"}
+
+            return;
+        }
+    }
+}
