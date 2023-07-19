c2ef47cdbabd7d6a828b13d2e55aed4dc03322c6
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-19 14:09:41 +0300

    Add files via upload

---
+++ Examples/Program.cs
@@ -0,0 +1,31 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            bool result = default;
+            char
+                number = '0',
+                upperLetter = 'A',
+                lowerLetter = 'a';
+            #endregion Items
+
+            // Info number:          0
+            // Info upperLetter:    'A'
+            // Info lowerLetter:    'a'
+
+            result = MyChar.CheckSymbol(number, checkByNumbers: true, checkByEnglishLetters: true);             // result = true
+            result = MyChar.CheckSymbol(number, checkByEnglishLetters: true);                                   // result = false
+            result = MyChar.CheckSymbol(upperLetter, checkByNumbers: true, checkByEnglishLetters: true);        // result = true
+            result = MyChar.CheckSymbol(upperLetter, checkByNumbers: true, checkByEnglishLowerLetters: true);   // result = false
+            result = MyChar.CheckSymbol(lowerLetter, checkByEnglishLetters: true);                              // result = true
+            result = MyChar.CheckSymbol(lowerLetter, checkByEnglishUpperLetters: true);                         // result = false
+
+            return;
+        }
+    }
+}
