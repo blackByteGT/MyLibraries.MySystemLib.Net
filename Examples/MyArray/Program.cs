529cda9f9e5192599e64644a5653e23d174473ed
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-19 13:46:59 +0300

    Add files via upload

---
+++ Examples/MyArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            bool result = default;
+            int[]
+                emptyArray = new int[] { },
+                notEmptyArray = new int[] { 0, 1, 2, 3, 4, 5 };
+            #endregion Items
+
+            // Start emptyList:     {}
+            // Start notEmptyList:  { 0, 1, 2, 3, 4, 5 }
+
+            result = MyArray.IsEmpty(emptyArray);       // result = true
+            result = MyArray.IsEmpty(notEmptyArray);    // result = false
+
+            return;
+        }
+    }
+}
