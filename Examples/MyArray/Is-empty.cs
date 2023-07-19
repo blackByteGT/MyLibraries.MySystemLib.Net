49258e280fbfbcea5b7f5631d0f3ee1fd18859f8
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-19 13:48:35 +0300

    Update Is-empty.cs

--- Examples/MyArray/Is-empty.cs
+++ Examples/MyArray/Is-empty.cs
@@ -13,8 +13,8 @@ namespace Examples
                 notEmptyArray = new int[] { 0, 1, 2, 3, 4, 5 };
             #endregion Items
 
-            // Start emptyList:     {}
-            // Start notEmptyList:  { 0, 1, 2, 3, 4, 5 }
+            // Start emptyArray:     {}
+            // Start notEmptyArray:  { 0, 1, 2, 3, 4, 5 }
 
             result = MyArray.IsEmpty(emptyArray);       // result = true
             result = MyArray.IsEmpty(notEmptyArray);    // result = false
