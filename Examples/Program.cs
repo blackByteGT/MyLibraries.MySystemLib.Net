487f961f51f846d71b0c257d7fced1d2d34ee780
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-19 14:59:31 +0300

    Add files via upload

---
+++ Examples/Program.cs
@@ -0,0 +1,18 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal number = default;
+
+            MyDecimal.Parse(ref number, "12.5"); // number = 12.5
+            number = default; MyDecimal.Parse(ref number, "12,5"); // number = 12.5
+            number = default; MyDecimal.Parse(ref number, "12/5"); // number = default
+
+            return;
+        }
+    }
+}
