cdf4fd1b37d25ee9b46388c2b1cdbdf63697ea84
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-19 15:27:48 +0300

    Update and rename Parse.cs to Parse.cs

---
+++ Examples/MyConvert/Parse.cs
@@ -0,0 +1,19 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace Examples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal number;
+
+            number = default; MyConvert.ConvertToDecimal(ref number, "12.5");                           // number = 12.5
+            number = default; MyConvert.ConvertToDecimal(ref number, "12,5");                           // number = 12.5
+            number = default; MyConvert.ConvertToDecimal(ref number, "12/5");                           // number = default
+            number = default; MyConvert.ConvertToDecimal(ref number, "12/5", new List<char>() { '/' }); // number = 12.5
+
+            return;
+        }
+    }
+}
