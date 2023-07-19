50ec4457ae1e264ac90dd02300b07e88e2065347
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-19 12:57:47 +0300

    Add files via upload

---
+++ Examples/MyList/Program.cs
@@ -0,0 +1,26 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            int[] 
+                arrayString = new int[] { 1, 2, 3, 4, 5 },
+                nullArray = null;
+            #endregion Items
+
+            // Start arrayString:   { 1, 2, 3, 4, 5 }
+            // Start nullArray:     null
+
+            MyArray.Add(ref nullArray, 0);                              // { 0 }
+            MyArray.Add(ref arrayString, 6);                            // { 1, 2, 3, 4, 5, 6 }
+            MyArray.Add(ref arrayString, 0, index: 0);                  // { 0, 1, 2, 3, 4, 5, 6 }
+            MyArray.Add(ref arrayString, 9, arrayString.Length / 2);    // { 0, 1, 2, 9, 3, 4, 5, 6 }
+
+            return;
+        }
+    }
+}
