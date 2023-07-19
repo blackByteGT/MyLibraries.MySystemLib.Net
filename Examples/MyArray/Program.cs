e6748804d8725ec56d24dfbf86454199b4f001f2
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-19 13:49:51 +0300

    Add files via upload

---
+++ Examples/MyArray/Program.cs
@@ -0,0 +1,25 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            bool result = default;
+            int[]
+                nullArray = null,
+                notNullArray = new int[] { 0, 1, 2, 3, 4, 5 };
+            #endregion Items
+
+            // Start nullArray:     null
+            // Start notNullArray:  { 0, 1, 2, 3, 4, 5 }
+
+            result = MyArray.IsNull(nullArray);       // result = true
+            result = MyArray.IsNull(notNullArray);    // result = false
+
+            return;
+        }
+    }
+}
