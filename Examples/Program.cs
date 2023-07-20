db91971aedc764231ea6631ab13c59c335f7f830
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-20 23:35:04 +0300

    Add files via upload

---
+++ Examples/Program.cs
@@ -0,0 +1,23 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            const decimal number = 12.5m;
+
+            bool result = default;
+            #endregion Items
+
+            // Info number: 12.5
+
+            result = MyDecimal.CheckPrecision(number, 0.1m);     // result = true
+            result = MyDecimal.CheckPrecision(number, 0.01m);    // result = false
+
+            return;
+        }
+    }
+}
