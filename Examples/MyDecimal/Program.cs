6bb2ba24148d8a1bb4cd151c44e4663442cf7931
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-21 22:18:47 +0300

    Add files via upload

---
+++ Examples/MyDecimal/Program.cs
@@ -0,0 +1,31 @@
+﻿using MyLibraries.MySystemLib.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            const decimal startNumber = 12.543m;
+
+            decimal number = default;
+            #endregion Items
+
+            // Info startNumber: 12.543
+
+            number = startNumber; MyDecimal.SetPrecision(ref number, 0.001m);   // number = 12.543
+            number = startNumber; MyDecimal.SetPrecision(ref number, 3);        // number = 12.543
+            number = startNumber; MyDecimal.SetPrecision(ref number, 0.01m);    // number = 12.54
+            number = startNumber; MyDecimal.SetPrecision(ref number, 2);        // number = 12.54
+            number = startNumber; MyDecimal.SetPrecision(ref number, 0.1m);     // number = 12.5
+            number = startNumber; MyDecimal.SetPrecision(ref number, 1);        // number = 12.5
+            number = startNumber; MyDecimal.SetPrecision(ref number, 1m);       // number = 12
+            number = startNumber; MyDecimal.SetPrecision(ref number, 0);        // number = 12
+
+            return;
+        }
+    }
+}
