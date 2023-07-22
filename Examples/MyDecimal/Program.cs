1ad22fae5b332f972116ca556f7f188697493b53
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-22 12:00:01 +0300

    Add files via upload

---
+++ Examples/MyDecimal/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int precision = default;
+
+            MyDecimal.GetPrecision(ref precision, 12.5m);                               // 1
+            MyDecimal.GetPrecision(ref precision, 12m);                                 // 0
+            MyDecimal.GetPrecision(ref precision, "23,7", new List<char>() { ',' });    // 1
+
+            return;
+        }
+    }
+}
