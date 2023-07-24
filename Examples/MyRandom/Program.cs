0417e6ff2b4d580b9e79116ef83180134f888c6e
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-24 14:59:53 +0300

    Add files via upload

---
+++ Examples/MyRandom/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            int currentValue = default;
+            List<int> list = 
+                new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            #endregion Items
+
+            // Info list: { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }
+
+            MyRandom.Next(ref currentValue, list); // 5
+            MyRandom.Next(ref currentValue, list); // 0
+            MyRandom.Next(ref currentValue, list); // 9
+
+            return;
+        }
+    }
+}
