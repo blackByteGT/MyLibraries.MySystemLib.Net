cdcc2258685aeae60f61590fefa63eabb0efe585
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-24 16:39:12 +0300

    Update Put-out-format.cs

--- Examples/MyString/Put-out-format.cs
+++ Examples/MyString/Put-out-format.cs
@@ -8,7 +8,6 @@ namespace Examples
         {
             string str;
 
-
             str = "123-45-67-890"; MyString.PutOutFormat(ref str, "XXX-XX-XX-XXX");                   // 1234567890
             str = "123.45.67.890"; MyString.PutOutFormat(ref str, "---.--.--.---", formatChar: '-');  // 1234567890
 
