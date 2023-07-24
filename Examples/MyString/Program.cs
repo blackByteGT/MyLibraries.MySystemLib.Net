bef8b7ce478ee4b9aa30cf11a1ce19d6aa1810e0
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-24 16:34:49 +0300

    Add files via upload

---
+++ Examples/MyString/Program.cs
@@ -0,0 +1,18 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+
+            str = "123-45-67-890"; MyString.PutOutFormat(ref str, "XXX-XX-XX-XXX");                   // 1234567890
+            str = "123.45.67.890"; MyString.PutOutFormat(ref str, "---.--.--.---", formatChar: '-');  // 1234567890
+
+            return;
+        }
+    }
+}
