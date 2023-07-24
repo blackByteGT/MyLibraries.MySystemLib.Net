13481bca88111158552c25315a62c0e89848227d
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-24 11:09:11 +0300

    Add files via upload

---
+++ Examples/MyDecimal/Program.cs
@@ -0,0 +1,16 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal number = default;
+
+            number = 1.0200m; MyDecimal.TakeExtraZerosFractionalPart(ref number); // 1.02
+
+            return;
+        }
+    }
+}
