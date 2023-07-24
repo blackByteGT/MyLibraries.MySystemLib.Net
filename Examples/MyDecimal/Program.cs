68eb70be87117661850ae1b3df4512eb9a09b2be
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-24 11:01:46 +0300

    Add files via upload

---
+++ Examples/MyDecimal/Program.cs
@@ -0,0 +1,17 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal number = default;
+
+            number = 12;        MyDecimal.TakeFractionalPart(ref number); // number = 12
+            number = 12.75m;    MyDecimal.TakeFractionalPart(ref number); // number = 12
+
+            return;
+        }
+    }
+}
