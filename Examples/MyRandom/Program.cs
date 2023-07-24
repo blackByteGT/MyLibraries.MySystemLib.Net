67418fffbe42ab70305bff1a50698f11cca3e2ea
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-24 14:31:46 +0300

    Add files via upload

---
+++ Examples/MyRandom/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            const int lengthStr = 5;
+
+            string str;
+            #endregion Items
+
+            // Info lengthStr: 5
+
+            str = default; MyRandom.Next(ref str, lengthStr, listAllowedChars: new List<char>() { 'h', 'e', 'l', 'l', 'o' });       // hoehl
+            str = default; MyRandom.Next(ref str, lengthStr, enterNumber: true);                                                    // str = 45426
+            str = default; MyRandom.Next(ref str, lengthStr, enterLowLatter: true);                                                 // joxtc
+            str = default; MyRandom.Next(ref str, lengthStr, enterCapLatter: true);                                                 // INJEO
+            str = default; MyRandom.Next(ref str, lengthStr, enterLowLatter: true, enterCapLatter: true);                           // fgEJL
+            str = default; MyRandom.Next(ref str, lengthStr * 2, enterNumber: true, enterLowLatter: true, enterCapLatter: true);    // HAzNarv85T
+            str = default; MyRandom.Next(ref str, lengthStr * 2, enterNumber: true, listAddChars: new List<char>() { '.', ',' });   // 517.42,627
+
+            return;
+        }
+    }
+}
