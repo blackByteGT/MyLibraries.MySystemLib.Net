b5ca5f699591435e374ae02683a7354f77a2e308
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-24 16:15:46 +0300

    Add files via upload

---
+++ Examples/MyString/Program.cs
@@ -0,0 +1,22 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            const string startStr = "1234567890";
+
+            string str;
+            #endregion Items
+
+            str = startStr; MyString.PutFormat(ref str, "XXX-XX-XX-XXX");                   // 123-45-67-890
+            str = startStr; MyString.PutFormat(ref str, "---.--.--.---", formatChar: '-');  // 123.45.67.890
+            str = startStr; MyString.PutFormat(ref str, "XXX-XX-XX-XXX-X");                 // 123-45-67-890
+
+            return;
+        }
+    }
+}
