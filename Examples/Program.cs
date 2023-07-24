0cccf3ec22dcb18eeb8a29aa4e5c1575b82769df
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-24 13:15:54 +0300

    Add files via upload

---
+++ Examples/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+
+            number = default; MyRandom.Next(ref number);                                // 154107982
+            number = default; MyRandom.Next(ref number, minNumber: 0, maxNumber: 10);   // 9
+            number = default; MyRandom.Next(ref number, maxNumber: 10);                 // 4
+
+            return;
+        }
+    }
+}
