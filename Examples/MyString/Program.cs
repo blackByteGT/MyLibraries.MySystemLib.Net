c294ad01ad8d0cab30bf8cf4b47464526e1cb203
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-24 17:00:07 +0300

    Add files via upload

---
+++ Examples/MyString/Program.cs
@@ -0,0 +1,29 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            const string str = "hello.everybody";
+
+            string substr = default;
+            #endregion Items
+
+            // Info str: "hello.everybody"
+
+            MyString.GetSubstring(ref substr, str);                         // "hello.everybody"
+            MyString.GetSubstring(ref substr, str, index: 6);               // "everybody"
+            MyString.GetSubstring(ref substr, str, lenght: 5);              // "hello"
+            MyString.GetSubstring(ref substr, str, index: 5, lenght: 1);    // "."
+
+            MyString.GetSubstring(ref substr, str, "lo", "bo");                                                     // ".every"
+            MyString.GetSubstring(ref substr, str, "lo", "bo", austerityPrimaryStr: true);                          // "lo.every"
+            MyString.GetSubstring(ref substr, str, "lo", "bo", austerityPrimaryStr: true, austerityFinalStr: true); // "lo.everybo"
+
+            return;
+        }
+    }
+}
