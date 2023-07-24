2be6702996066d29d0d623dca61ec37084587a7b
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-24 10:31:09 +0300

    Add files via upload

---
+++ Examples/MyDecimal/Program.cs
@@ -0,0 +1,19 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int digit = default;
+
+            MyDecimal.GetDigit(ref digit, 1);       // digit = 1
+            MyDecimal.GetDigit(ref digit, 10);      // digit = 2
+            MyDecimal.GetDigit(ref digit, 100);     // digit = 3
+            MyDecimal.GetDigit(ref digit, 1000);    // digit = 4
+
+            return;
+        }
+    }
+}
