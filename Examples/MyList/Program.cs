5b08459f5066c29de5dabbed76c7bee8f4cf72b6
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-24 14:52:58 +0300

    Add files via upload

---
+++ Examples/MyList/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = 
+                new List<int>() { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+            MyList.Shuffle(ref list); // { 2, 7, 3, 4, 9, 8, 5, 0, 6, 1 }
+
+            return;
+        }
+    }
+}
