79ed8a29df8764b9d7c32c10bd8e3608c9c2c4cd
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-24 11:35:22 +0300

    Add files via upload

---
+++ Examples/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyException exception = default; GetException(ref exception);
+
+            return;
+        }
+
+        static void GetException(ref MyException exception)
+        {
+            try
+            {
+                throw new Exception();
+            }
+            catch (Exception e)
+            {
+                exception = new MyException(true, e);
+
+                /*
+                 exception:
+                    NumberLine = 23
+                    MemberName = GetException
+                    FilePath = "...\Examples\Program.cs"
+                    ExceptionInfo = e
+                 */
+            }
+        }
+    }
+}
