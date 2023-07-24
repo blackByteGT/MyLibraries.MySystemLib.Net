f183432206208fb9575efd562a8faaee0a45bab8
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-24 15:25:10 +0300

    Update Set-length.cs

--- Examples/MyString/Set-length.cs
+++ Examples/MyString/Set-length.cs
@@ -13,6 +13,7 @@ namespace Examples
 
             str = startStr; MyString.SetLength(ref str, str.Length + 2);        // "hello  "
             str = startStr; MyString.SetLength(ref str, str.Length + 2, '_');   // "hello__"
+            str = startStr; MyString.SetLength(ref str, str.Length - 2);        // "hel"
 
             return;
         }
