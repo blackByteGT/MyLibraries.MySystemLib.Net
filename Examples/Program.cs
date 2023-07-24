eb2ea70631672b78dff9f9c11dd1e2e103e21047
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-24 15:20:35 +0300

    Add files via upload

---
+++ Examples/Program.cs
@@ -0,0 +1,20 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            const string startStr = "hello";
+            string str;
+            #endregion Items
+
+            str = startStr; MyString.SetLength(ref str, str.Length + 2);        // "hello  "
+            str = startStr; MyString.SetLength(ref str, str.Length + 2, '_');   // "hello__"
+
+            return;
+        }
+    }
+}
