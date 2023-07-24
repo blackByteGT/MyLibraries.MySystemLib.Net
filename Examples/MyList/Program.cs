49c55804af631577686a43be820b7f3b2249ce2f
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-24 14:47:29 +0300

    Add files via upload

---
+++ Examples/MyList/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            List<int> list = new List<int>() { 0, 1, 2, 3, 4 };
+            int countList = list.Count;
+            #endregion Items
+
+            // Start list: { 0, 1, 2, 3, 4 }
+
+            for (int i = 0; i < 2; i++)
+                MyList.Move(ref list, i, countList - i - 1);
+
+            // i = 0: { 4, 1, 2, 3, 0 }
+            // i = 1: { 4, 3, 2, 1, 0 }
+
+            return;
+        }
+    }
+}
