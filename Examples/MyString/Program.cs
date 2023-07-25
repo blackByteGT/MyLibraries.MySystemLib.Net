df2e7a2eeeaa2b85a478e0a1da63683526956d66
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-25 14:25:31 +0300

    Add files via upload

---
+++ Examples/MyString/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> listSubstrings;
+
+            listSubstrings = default; MyString.GetSubstrings(ref listSubstrings, "1.2.3.4.5", ".");        // { "1", "2", "3", "4", "5" }
+            listSubstrings = default; MyString.GetSubstrings(ref listSubstrings, "1.2.3.4.5.", ".");       // { "1", "2", "3", "4", "5" }
+            listSubstrings = default; MyString.GetSubstrings(ref listSubstrings, "1..2..3..4..5.", "..");  // { "1", "2", "3", "4", "5." }
+
+            return;
+        }
+    }
+}
