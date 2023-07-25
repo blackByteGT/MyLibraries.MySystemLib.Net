560f18cd134a2a716d6c11362d72e727c7cadccb
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-25 18:42:21 +0300

    Add files via upload

---
+++ Examples/MyString/Program.cs
@@ -0,0 +1,29 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string startStr, str;
+
+            startStr = "abcde";
+
+            str = startStr; MyString.Remove(ref str, startIndex: 0);                            // "bcde"
+            str = startStr; MyString.Remove(ref str, startIndex: 2, endIndex: str.Length - 1);  // "ab"
+
+            startStr = "ababab";
+
+            str = startStr; MyString.Remove(ref str, 'a');                      // "bbb"
+            str = startStr; MyString.Remove(ref str, 'a', austerity: false);    // "babab"
+
+            startStr = "accaacca";
+
+            str = startStr; MyString.Remove(ref str, "ca");                     // "ac"
+            str = startStr; MyString.Remove(ref str, "ca", austerity: false);   // "acacca"
+
+            return;
+        }
+    }
+}
