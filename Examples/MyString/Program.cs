2596be3dbdc768a98e53a52309be699096ff160f
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-25 15:50:14 +0300

    Add files via upload

---
+++ Examples/MyString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            const string str = "abcde";
+
+            bool result = default;
+            #endregion Items
+
+            result = MyString.IsEntry(str, 'e');  // true
+            result = MyString.IsEntry(str, 'q');  // false
+            result = MyString.IsEntry(str, "ab"); // true
+
+            result = MyString.IsEntry(str, format: "XXcXX");                      // true
+            result = MyString.IsEntry(str, format: "--c--", formatChar: '-');     // true
+            result = MyString.IsEntry(str, format: "--c--f", formatChar: '-');    // false
+
+            result = MyString.IsEntry(str, new List<string>() { "a", "ab", "tr" });                         // true
+            result = MyString.IsEntry(str, new List<string>() { "a", "ab", "tr" }, austerityEntry: true);   // false
+
+            return;
+        }
+    }
+}
