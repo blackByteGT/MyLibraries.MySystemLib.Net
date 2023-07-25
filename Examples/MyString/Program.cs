49ad6debc025598a02bb1421f008d2ae5c374e09
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-25 16:00:31 +0300

    Add files via upload

---
+++ Examples/MyString/Program.cs
@@ -0,0 +1,20 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool result;
+
+            result = MyString.IsEmpty("abcde");                     // false
+            result = MyString.IsEmpty(null);                        // true
+            result = MyString.IsEmpty(default);                     // true
+            result = MyString.IsEmpty("abcde", whiteSpace: true);   // false
+            result = MyString.IsEmpty("   ", whiteSpace: true);     // true
+
+            return;
+        }
+    }
+}
