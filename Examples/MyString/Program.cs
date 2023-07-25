9231b05bec9e541e5f21613754fc0e2feacd6482
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-25 14:54:22 +0300

    Add files via upload

---
+++ Examples/MyString/Program.cs
@@ -0,0 +1,33 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Items
+            const int defaultIndex = -1;
+            const string str = "abcde";
+
+            int
+                startIndex,
+                endIndex;
+            #endregion Items
+
+            // Info str: "abcde"
+            // Info defaultIndex: -1
+
+            startIndex = endIndex = defaultIndex;
+            MyString.GetSubstringIndices(ref startIndex, ref endIndex, str, "q"); // startIndex = endIndex = -1
+
+            startIndex = endIndex = defaultIndex; 
+            MyString.GetSubstringIndices(ref startIndex, ref endIndex, str, "c"); // startIndex = endIndex = 2
+
+            startIndex = endIndex = defaultIndex;
+            MyString.GetSubstringIndices(ref startIndex, ref endIndex, str, "bcd"); // startIndex = 1, endIndex = 3
+
+            return;
+        }
+    }
+}
