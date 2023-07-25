55190ec6b7d75cd08f7611234060383081463f6c
Vladislav Rak <130608521+blackByteGT@users.noreply.github.com>
2023-07-25 14:36:28 +0300

    Add files via upload

---
+++ Examples/MyString/Program.cs
@@ -0,0 +1,17 @@
+﻿using MyLibraries.MySystemLib.Classes;
+
+namespace ConsoleAppForShowExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str;
+
+            str = "hello"; MyString.Flip(ref str);      // "olleh"
+            str = "everybody"; MyString.Flip(ref str);  // "ydobyreve"
+
+            return;
+        }
+    }
+}
